400fab277669802978a72c15ebdc5e818c052f30
Andrey <rogatin-av@ya.ru>
2025-06-16 16:05:32 +0300

    Add handler

---
+++ src/main/java/hexlet/code/handler/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package hexlet.code.handler;
+
+import hexlet.code.exception.ResourceDeletionException;
+import hexlet.code.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ResourceDeletionException.class)
+    public ResponseEntity<String> handleResourceDeleteException(ResourceDeletionException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
